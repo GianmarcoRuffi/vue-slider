1c6def85ee7d660d850bf0da39f330a1be089358
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-21 16:59:02 +0200

    Aggiornamento vue

--- js/script.js
+++ js/script.js
@@ -45,110 +45,110 @@ const app = new Vue({
   mounted() {},
 });
 
-const title = ["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
+// const title = ["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
 
-const text = [
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
-  "Lorem ipsum",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
-  "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
-];
+// const text = [
+//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+//   "Lorem ipsum",
+//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+//   "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+// ];
 
-//variabile per raccogliere tutto l'html che va in items-container
-let itemTemplate = "";
+// //variabile per raccogliere tutto l'html che va in items-container
+// let itemTemplate = "";
 
-//variabile per raccogliere tutto l'html che va in thumbs-container
-let thumbTemplate = "";
+// //variabile per raccogliere tutto l'html che va in thumbs-container
+// let thumbTemplate = "";
 
-// preparo una variabile con l'indice dell'elemento attivo e la pongo inizialmente a 0 ovvero il primo elemento dell'array
-let currentIndexActive = 0;
+// // preparo una variabile con l'indice dell'elemento attivo e la pongo inizialmente a 0 ovvero il primo elemento dell'array
+// let currentIndexActive = 0;
 
-//eseguo il ciclo for sull'array delle immagini (items) e popolo l'html delle due varibaili da stampare nei due contenitori (immagini e thumbnails)
-// for (let i = 0; i < items.length; i++) {
-//   let classActive = "";
-//   if (i === currentIndexActive) {
-//     classActive = "active";
+// //eseguo il ciclo for sull'array delle immagini (items) e popolo l'html delle due varibaili da stampare nei due contenitori (immagini e thumbnails)
+// // for (let i = 0; i < items.length; i++) {
+// //   let classActive = "";
+// //   if (i === currentIndexActive) {
+// //     classActive = "active";
+// //   }
+// //   itemTemplate += `
+// //   <div class="item ${classActive}">
+// //     <img src="${items[i].img}" />
+// //       <div class="title">
+// //         <h2>${items[i].title}</h2>
+// //         <p>${items[i].text}</p>
+// //       </div>
+// //   </div>`;
+// //   thumbTemplate += `
+// //   <div class="thumb ${classActive}">
+// //     <img src="${items[i].img}" alt="${items[i].title}" />
+// //   </div>`;
+// // }
+// //console.log(thumbTemplate);
+
+// // metto in due variabili rispettivamente i contenitori che si trovano nell'html
+// const itemsContainer = document.querySelector(".items-container");
+// const thumbsContainer = document.querySelector(".thumbs-container");
+// //console.log(itemContainer);
+
+// //stampo l'html corrispondente nei due contenitori
+// itemsContainer.innerHTML = itemTemplate;
+// thumbsContainer.innerHTML += thumbTemplate;
+// //document.querySelector(".item").classList.add("active");
+
+// //Pulsanti
+// //.next .fa-circle-chevron-down
+// //.prev .fa-circle-chevron-up
+// //metto nelle variabili next e prev i due pulsanti
+// const next = document.querySelector(" .fa-circle-chevron-down");
+// const prev = document.querySelector(" .fa-circle-chevron-up");
+// //console.log(next, prev);
+
+// function slideDown() {
+//   //prendere immagine con currentIndexActive e togliere classe active
+//   const imgs = document.getElementsByClassName("item");
+//   imgs[currentIndexActive].classList.remove("active");
+//   const thumbs = document.getElementsByClassName("thumb");
+//   thumbs[currentIndexActive].classList.remove("active");
+
+//   //console.log(imgs);
+//   if (currentIndexActive === 4) {
+//     currentIndexActive = 0;
+//   } else {
+//     currentIndexActive++;
 //   }
-//   itemTemplate += `
-//   <div class="item ${classActive}">
-//     <img src="${items[i].img}" />
-//       <div class="title">
-//         <h2>${items[i].title}</h2>
-//         <p>${items[i].text}</p>
-//       </div>
-//   </div>`;
-//   thumbTemplate += `
-//   <div class="thumb ${classActive}">
-//     <img src="${items[i].img}" alt="${items[i].title}" />
-//   </div>`;
+//   //console.log(currentIndexActive);
+//   imgs[currentIndexActive].classList.add("active", "w3-animate-opacity");
+//   //console.log(currentIndexActive);
+//   thumbs[currentIndexActive].classList.add("active");
 // }
-//console.log(thumbTemplate);
+// function slideUp() {
+//   const imgs = document.getElementsByClassName("item");
+//   imgs[currentIndexActive].classList.remove("active");
+//   const thumbs = document.getElementsByClassName("thumb");
+//   thumbs[currentIndexActive].classList.remove("active");
+//   //console.log(imgs);
+//   if (currentIndexActive === 0) {
+//     currentIndexActive = items.length - 1;
+//   } else {
+//     currentIndexActive--;
+//   }
+//   //console.log(currentIndexActive);
+//   imgs[currentIndexActive].classList.add("active");
+//   //console.log(currentIndexActive);
+//   thumbs[currentIndexActive].classList.add("active");
+// }
 
-// metto in due variabili rispettivamente i contenitori che si trovano nell'html
-const itemsContainer = document.querySelector(".items-container");
-const thumbsContainer = document.querySelector(".thumbs-container");
-//console.log(itemContainer);
+// next.addEventListener("click", slideDown);
+// prev.addEventListener("click", slideUp);
 
-//stampo l'html corrispondente nei due contenitori
-itemsContainer.innerHTML = itemTemplate;
-thumbsContainer.innerHTML += thumbTemplate;
-//document.querySelector(".item").classList.add("active");
+// // // Funzione start scorrimento slider al caricamento della pagina
 
-//Pulsanti
-//.next .fa-circle-chevron-down
-//.prev .fa-circle-chevron-up
-//metto nelle variabili next e prev i due pulsanti
-const next = document.querySelector(" .fa-circle-chevron-down");
-const prev = document.querySelector(" .fa-circle-chevron-up");
-//console.log(next, prev);
+// // const startSlider = setInterval(slideDown, 3000);
 
-function slideDown() {
-  //prendere immagine con currentIndexActive e togliere classe active
-  const imgs = document.getElementsByClassName("item");
-  imgs[currentIndexActive].classList.remove("active");
-  const thumbs = document.getElementsByClassName("thumb");
-  thumbs[currentIndexActive].classList.remove("active");
+// // // Funzione stop scorrimento slider
 
-  //console.log(imgs);
-  if (currentIndexActive === 4) {
-    currentIndexActive = 0;
-  } else {
-    currentIndexActive++;
-  }
-  //console.log(currentIndexActive);
-  imgs[currentIndexActive].classList.add("active", "w3-animate-opacity");
-  //console.log(currentIndexActive);
-  thumbs[currentIndexActive].classList.add("active");
-}
-function slideUp() {
-  const imgs = document.getElementsByClassName("item");
-  imgs[currentIndexActive].classList.remove("active");
-  const thumbs = document.getElementsByClassName("thumb");
-  thumbs[currentIndexActive].classList.remove("active");
-  //console.log(imgs);
-  if (currentIndexActive === 0) {
-    currentIndexActive = items.length - 1;
-  } else {
-    currentIndexActive--;
-  }
-  //console.log(currentIndexActive);
-  imgs[currentIndexActive].classList.add("active");
-  //console.log(currentIndexActive);
-  thumbs[currentIndexActive].classList.add("active");
-}
-
-next.addEventListener("click", slideDown);
-prev.addEventListener("click", slideUp);
-
-// // Funzione start scorrimento slider al caricamento della pagina
-
-// const startSlider = setInterval(slideDown, 3000);
-
-// // Funzione stop scorrimento slider
-
-// document
-//   .querySelector(".items-container")
-//   .addEventListener("click", function () {
-//     clearInterval(startSlider);
-//   });
+// // document
+// //   .querySelector(".items-container")
+// //   .addEventListener("click", function () {
+// //     clearInterval(startSlider);
+// //   });
